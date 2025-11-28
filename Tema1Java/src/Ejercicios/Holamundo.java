7598d0b95421aa473ff39f7974e7977c87d11773
RCEE <RCEE@D13-EQ08.iesam.net>
2025-11-28 10:49:13 +0100

    Añadi una funcion suma

---
+++ Tema1Java/src/Ejercicios/Holamundo.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola mundo");
+	}
+public static int suma (int a, int b) {
+	return a + b;
+}
+}
